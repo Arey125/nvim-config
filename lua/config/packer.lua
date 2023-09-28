914f68f8a957c60ff8c4c9d5267dace9a2ab7907
Andrey Kurenkov <andrej.kurenckoff@gmail.com>
2023-09-28 17:42:48 +0300

    feat: lualine

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -59,5 +59,10 @@ return require('packer').startup(function(use)
   }
 
   use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
 end)
 
