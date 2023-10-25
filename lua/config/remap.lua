a075b2c8f87ea533cf4b3206c2f2a5934443ce56
Andrey Kurenkov <andrej.kurenckoff@gmail.com>
2023-10-25 12:52:15 +0300

    feat: git push remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -14,3 +14,4 @@ vim.keymap.set("n", "<leader>ce", ":CodeiumEnable<CR>")
 vim.keymap.set("n", "<leader>cd", ":CodeiumDisable<CR>")
 
 vim.keymap.set("n", "<leader>g", ":G<CR>")
+vim.keymap.set("n", "<leader>gp", ":G push<CR>")
