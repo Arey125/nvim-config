2229da61d53141161e0fd1af822b38bba1089215
Arey125 <andrej.kurenckoff@gmail.com>
2023-10-30 22:40:50 +0300

    feat: fugitive remap and coq_settings

--- after/plugin/coq.lua
+++ after/plugin/coq.lua
@@ -1,4 +1,4 @@
-vim.cmd(':COQnow')
+vim.cmd(':COQnow --shut-up')
 
 vim.g.coq_settings = {
   keymap = {
--- ftplugin/fugitive.lua
+++ ftplugin/fugitive.lua
@@ -4,3 +4,5 @@ vim.keymap.set("n", "<leader>f", ":G fetch<CR>")
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>c', builtin.git_branches, {})
+
+vim.keymap.set("n", "q", ":q<CR>")
