ddb41dddcd087cfc8a4fe9b6165dbead6694a5f1
Arey125 <andrej.kurenckoff@gmail.com>
2023-10-30 22:57:51 +0300

    feat: telescope search

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -15,7 +15,7 @@ require('telescope').setup{
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>s', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
