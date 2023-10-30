0654c882befa8f7cf111cb4e3741e694cf6d94b2
Arey125 <andrej.kurenckoff@gmail.com>
2023-10-30 22:52:17 +0300

    feat: push

--- ftplugin/fugitive.lua
+++ ftplugin/fugitive.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>p", ":G push<CR>")
+vim.keymap.set("n", "<leader>pu", ":G push<CR>")
 vim.keymap.set("n", "<leader>f", ":G fetch<CR>")
 
 local builtin = require('telescope.builtin')
