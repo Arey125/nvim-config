3662034049837db0f5b5c85445867e005fe593fd
Andrey Kurenkov <andrej.kurenckoff@gmail.com>
2023-12-28 13:02:58 +0300

    feat: find selected

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -20,9 +20,27 @@ require('telescope').setup{
   }
 }
 
+function vim.getVisualSelection()
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg('v')
+	vim.fn.setreg('v', {})
+
+	text = string.gsub(text, "\n", "")
+	if #text > 0 then
+		return text
+	else
+		return ''
+	end
+end
+
+
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('v', '<space>f', function()
+	local text = vim.getVisualSelection()
+	builtin.live_grep({ default_text = text })
+end, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
